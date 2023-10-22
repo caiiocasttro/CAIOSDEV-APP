023203991e05fa8fa9a4830500e82cf09ca81202
Caio Chaves <caiojob@outlook.pt>
2023-10-22 21:15:15 +0200

    Corrections
    Corrections!

--- Caiosdev-Resume/Caiosdev-Resume/AppDelegate.swift
+++ Caiosdev-Resume/Caiosdev-Resume/AppDelegate.swift
@@ -15,7 +15,7 @@ var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        Thread.sleep(forTimeInterval: 2.0)
+        Thread.sleep(forTimeInterval: 1.0)
         
         if #available(iOS 15.0, *) {
             window?.overrideUserInterfaceStyle = .light
